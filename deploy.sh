4474c6aab9a5eb75ccd2bc87bf3e45ae1107722f
MooGoo <moogoo78@gmail.com>
2020-11-14 03:53:29 +0800

    edited source

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # make html
-cd docs; make html
+pushd docs; make html
 
 # git commit 
+popd
 git add .; git ci 'edited source'; git push
 
 # deploy
-cd ../gh-pages/html; git add .;git ci 'auto';git push origin gh-pages
+cd ../gh-pages/html; git add .;git ci 'auto';git push origin gh-pages:w
+
